3717391bed7b8e3d9933d8c915b43003a73d5e77
Venneti Sri Satya Vinay <81536213+srisatyavinay@users.noreply.github.com>
2022-04-13 21:48:20 +0530

    Add final exp-7 files

--- experiment/simulation/js/inverter.js
+++ experiment/simulation/js/inverter.js
@@ -1,71 +1,50 @@
-function inverter(){
-    if(count7%2 == 1 ){
-        if(list_input[0].input)
-        {
-            document.getElementById("output-box").innerHTML = "Output is 0 "
-        }
-        else
-        {
-            document.getElementById("output-box").innerHTML = "Output is 1 "
-        }
-       
-      
-    }
-    if(count7%2 == 0 ){
-        if(list_input[0].input)
-        {
-            document.getElementById("output-box").innerHTML = "Output is 1 "
-        }
-        else
-        {
-            document.getElementById("output-box").innerHTML = "Output is 0 "
-        }
-      
+function permutator(inputArr) {
+    const results = [];
 
+    function permute(arr, memo) {
+        let cur
+
+        memo = memo || [];
+
+        for (let i = 0; i < arr.length; i++) {
+            cur = arr.splice(i, 1);
+            if (arr.length === 0) {
+                results.push(memo.concat(cur));
+            }
+            permute(arr.slice(), memo.concat(cur));
+            arr.splice(i, 0, cur[0]);
+        }
+
+        return results;
     }
-    invertervalidate()
+
+    return permute(inputArr);
 }
 
-function permutator(inputArr) {
-    var results = [];
-  
-    function permute(arr, memo) {
-      var cur, memo = memo || [];
-  
-      for (var i = 0; i < arr.length; i++) {
-        cur = arr.splice(i, 1);
-        if (arr.length === 0) {
-          results.push(memo.concat(cur));
-        }
-        permute(arr.slice(), memo.concat(cur));
-        arr.splice(i, 0, cur[0]);
-      }
-  
-      return results;
-    }
-  
-    return permute(inputArr);
-  }
-
-function invertervalidate()
-{
-    x = permutator([0, 1, 2, 3, 4])
+function inverterValidate() {
+    const x = permutator([0, 1, 2, 3, 4])
     // console.log(x)
-    circuit_valid = 0
+    circuitValid = 0
     for (let i = 0; i < x.length; i++) {
-        if(jsmap.has("input0$inverter" + x[i][0]) && jsmap.has("inverter" + x[i][0] + "$inverter" + x[i][1]) && jsmap.has("inverter" + x[i][1] + "$inverter" + x[i][2]) && jsmap.has("inverter" + x[i][2] + "$inverter" + x[i][3]) && jsmap.has("inverter" + x[i][3] + "$inverter" + x[i][4]) && jsmap.has("inverter" + x[i][4] + "$output0")){
-            circuit_valid = 1
+        if (connectionMap.has("input0$inverter" + x[i][0]) && connectionMap.has("inverter" + x[i][0] + "$inverter" + x[i][1]) && connectionMap.has("inverter" + x[i][1] + "$inverter" + x[i][2]) && connectionMap.has("inverter" + x[i][2] + "$inverter" + x[i][3]) && connectionMap.has("inverter" + x[i][3] + "$inverter" + x[i][4]) && connectionMap.has("inverter" + x[i][4] + "$output0")) {
+            circuitValid = 1
             break
         }
     }
-    if(circuit_valid)
-    {
-        document.getElementById("graph-image").src = "./images/Screenshot (169).png" 
-        document.getElementById("graph-image").style.display = "block";      
+    document.getElementById('error-container').style = 'display:none;';
+    if (circuitValid) {
+        document.getElementById("graph-image").src = "./images/Screenshot (169).png"
+        document.getElementById("graph-image").style.display = "block";
+        changeObservation("&#10004; Circuit is correct", 'text-danger', 'text-success')
     }
-    else
-    {  
+    else {
         document.getElementById("graph-image").style.display = "none";
-        alert("The circuit is wrong");
+        changeObservation("&#10060; Circuit is incorrect", 'text-success', 'text-danger')
     }
+}
+
+function changeObservation(htmlText, removedClass, addedClass) {
+    document.getElementById("output-text").innerHTML = htmlText
+    document.getElementById("output-text").classList.remove(removedClass)
+    document.getElementById("output-text").classList.add(addedClass)
 }
