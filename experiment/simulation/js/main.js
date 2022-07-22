8b9a764a83cd87b2a4d0f2746001ef2b28eda833
Venneti Sri Satya Vinay <81536213+srisatyavinay@users.noreply.github.com>
2022-07-22 15:00:30 +0530

    Exp - 5 Code review (#6)
    * Add all files
    
    * Make a change
    
    * Add images
    
    * Correct a bug
    
    * Add final exp-7 files
    
    * Make the changes suggested
    
    * Final exp - 5
    
    * Final exp - 5
    
    * Demo created
    
    * practice section fixed
    
    * updated experiment desciptor
    
    * code refactoring animation fixed
    
    * fixed animation and mobile view
    
    Co-authored-by: Aditya <aadityamalhotra1512@gmail.com>

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,40 @@
-//Your JavaScript goes in here
+'use strict';
+import { comp2Input0, comp2Output, resetCounts } from "./integrate.js";
+import { jsplumbInstance, editConnectionMap } from "./components.js";
+
+export const connectionMap = new Map();
+export let componentsList = [];
+const EMPTY="";
+const container = document.getElementById("diagram");
+container.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+});
+window.refreshWorkingArea = refreshWorkingArea;
+
+function emptyList() {
+    for(const component of componentsList) {
+        let elem = document.getElementById(component.id);
+        elem.parentNode.removeChild(elem);
+    }
+    componentsList = [];
+}
+export function refreshObservations() {
+    // refresh the errors
+    document.getElementById("error-container").style.display="none";   
+    document.getElementById("output-text").innerHTML = EMPTY;
+    document.getElementById("graph-image").style.display = "none";
+}
+
+export function refreshWorkingArea() {
+    // to reset the working area
+    jsplumbInstance.deleteEveryEndpoint();
+    editConnectionMap();
+
+    // to remove all the svgs called in the working area
+    emptyList();
+    resetCounts();
+    comp2Input0();
+    comp2Output();
+    refreshObservations();
+}
+refreshWorkingArea();
